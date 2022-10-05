fff7b4514af224e929a54fa72d1c2523705ffb23
CetusWhale <yi-xuan_yu24@milton.edu>
2022-10-04 23:20:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] name = {"James", "Andrew", "Leon", "Heather", "Zack"};
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+        }
+      
+    }
+}
